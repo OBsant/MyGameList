043d6b552cbbb06d24017cbe4c13a1b721706358
Bruno <ocivaldobruno.nascimento@gmail.com>
2023-07-16 18:07:50 -0300

    ajustes

--- MyGameList/src/app/layout.tsx
+++ MyGameList/src/app/layout.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import Navbar from "@/components/Navbar";
 import "./globals.css";
 import type { Metadata } from "next";
--- MyGameList/src/components/Navbar.tsx
+++ MyGameList/src/components/Navbar.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { getAuth, signOut } from "firebase/auth";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
